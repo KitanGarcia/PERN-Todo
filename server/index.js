f017fc836f7ce29602e2105f0d200c68c1d7deb6
Kitan Garcia <kitangar@gmail.com>
2020-05-04 23:30:49 -0700

    create POST to create todo; GET to get all todos

--- server/index.js
+++ server/index.js
@@ -8,11 +8,33 @@ app.use(cors());
 app.use(express.json());//gives access to request.body
 
 //ROUTES
-//
 //create a todo
-//
+app.post('/todos', async(req, res) => {
+  try {
+    console.log(req.body);
+    const { description } = req.body;
+    const newTodo = await pool.query("INSERT INTO todo () (description) VALUES($1) RETURNING *", [description]
+    );
+    //insert Value 1 (which is description) into todo table in the description column
+    //RETURNING * returns back the data in the database
+    res.json(newTodo.rows[0]);
+    res.json(newTodo);
+
+  } catch (err) {
+    console.error(err.message);
+  }
+});
+
 //get all todos
-//
+app.get('/todos', async(req, res) => {
+  try {
+    const allTodos = await pool.query("SELECT * FROM todo");
+    res.json(allTodos.rows);
+  }
+  catch (err) {
+    console.error(message);
+  }
+});
 //get a todo
 //
 //update a todo
