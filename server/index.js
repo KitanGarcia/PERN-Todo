feb0158bd56738e42683ec9aae7f1098d6630e74
Kitan Garcia <kitangar@gmail.com>
2020-09-02 18:21:38 -0700

    complete update and get by ID express routes

--- server/index.js
+++ server/index.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
-const pool = require('./db');//connect to the db
+const pool = require('./db'); //connect to the db
 
 //middleware
-app.use(cors());//allows different domain applications to interact with each other
-app.use(express.json());//gives access to req.body
+app.use(cors()); //allows different domain applications to interact with each other
+app.use(express.json()); //gives access to req.body
 
 //ROUTES
 
@@ -19,7 +19,7 @@ app.post('/todos', async(req, res) => {
     //RETURNING * returns back the data in the database
     
     res.json(newTodo.rows[0]);
-//    res.json(newTodo);
+    //res.json(newTodo);
 
   } catch (err) {
     console.error(err.message);
@@ -30,7 +30,8 @@ app.post('/todos', async(req, res) => {
 app.get('/todos', async(req, res) => {
   try {
     const allTodos = await pool.query("SELECT * FROM todo");
-    res.json(allTodos.rows);
+    //console.log(allTodos.rows);
+    res.json(allTodos.rows); //sends to back to client
   }
   catch (err) {
     console.error(message);
@@ -38,10 +39,15 @@ app.get('/todos', async(req, res) => {
 });
 
 //get a todo
-//STILL IN PROGRESS
-app.get('/todo', async(req, res) => {
+app.get('/todos/:id', async(req, res) => {
   try {
-    const todo = await pool.query("SELECT ");//select todo ID from table
+    console.log(req.params); //will log what client sends as ID
+    //for above, use http://localhost:5000/todos/{id#} on clientside to observe
+
+    const {id} = req.params;
+    const todo = await pool.query("SELECT * FROM todo WHERE todo_id = $1", [id]); //select todo ID from table matching the ID parameterized by the client
+
+    res.json(todo.rows[0]);
   }
   catch(err) {
     console.error(err.message)
@@ -49,6 +55,21 @@ app.get('/todo', async(req, res) => {
 });
 
 //update a todo
+app.put('/todos/:id', async(req, res) => {
+  try {
+    //console.log(req.params); //will log what client sends as ID
+    //for above, use http://localhost:5000/todos/{id#} on clientside to observe
+
+    const {id} = req.params;
+    const {description} = req.body;
+    const updateTodo = await pool.query("UPDATE todo SET description = $1 WHERE todo_id = $2", [description, id]); //Update a todo description based on todo_id
+
+    res.json("Todo " + id + " has been updated with: " + description);
+  }
+  catch(err) {
+    console.error(err.message)
+  }
+});
 
 app.listen(5000, () => {
   console.log("Server has started on port 5000");
