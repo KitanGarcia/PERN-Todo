95bbfb3f6acb7ff166d9a6329d8fdb778a67e2cc
Kitan Garcia <kitangar@gmail.com>
2020-09-19 23:05:41 -0700

    complete Todo Component

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,18 @@
 import React, {Fragment} from 'react';
 import './App.css';
 
+//components
+import InputTodo from './components/InputTodo.js';
+import ListTodos from './components/ListTodos.js';
+import EditTodo from './components/EditTodo.js';
+
 function App() {
   return (
-    <Fragment></Fragment>
+    <Fragment>
+      <div className = "container">
+        <InputTodo />
+      </div>
+    </Fragment>
   );
 }
 
